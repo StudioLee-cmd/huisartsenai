9ce25f4320c6abf99cb183f3ba90ec73ce9a937c
Tim Van der lee <timvanderlee@MacBook-Air-van-Tim.local>
2026-02-06 15:13:58 +0100

    fix: reduce mobile logo size while keeping desktop unchanged

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -31,7 +31,7 @@ const Header: React.FC = () => {
                             alt={siteDetails.siteName}
                             width={400}
                             height={120}
-                            className="h-36 md:h-48 w-auto object-contain"
+                            className="h-20 md:h-48 w-auto object-contain"
                         />
                     </Link>
 
